7036608497853014edbc5ca4c054e2a13faf5a29
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-02-20 14:32:45 +0100

    Revert "pie charts"
    This reverts commit 79fd498e08f84ba3dade4c6c0e0d34f403a643ae.

--- todo/src/components/DashboardLayoutBasic.js
+++ todo/src/components/DashboardLayoutBasic.js
@@ -32,8 +32,6 @@ import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import LineChartData from './LineChartData';
-import PieChartCategoriesData from './PieChartCategoriesData';
-import PieChartTasksData from './PieChartTasksData';
 dayjs.extend(isSameOrAfter);
 
 
@@ -235,7 +233,7 @@ export default function DashboardLayoutBasic(props) {
 
   const Box = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? 'rgba(10,10,10)' : 'rgba(245,245,245)',
-    marginLeft: '2%',
+     marginLeft: '2%',
     webkitBackdropFilter: 'blur(5px)',
     backdropFilter: 'blur(10px)',
     display: 'flex',
@@ -264,25 +262,8 @@ export default function DashboardLayoutBasic(props) {
     boxShadow: theme.palette.mode === 'dark' ? '0 4px 10px rgba(0, 0, 0, 0.6)' : '0 4px 10px rgba(0, 0, 0, 0.1)',
   }));
 
-  const ChartsBox = styled('div')(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(10,10,10)' : 'rgba(245,245,245)',
-    marginLeft: '2%',
-    webkitBackdropFilter: 'blur(5px)',
-    backdropFilter: 'blur(10px)',
-    display: 'flex',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    borderRadius: '25px',
-    color: theme.palette.mode === 'light' ? '#000000' : 'rgba(245,245,245)',
-    marginTop: '5%',
-    boxShadow: theme.palette.mode === 'dark' ? '0 4px 10px rgba(0, 0, 0, 0.6)' : '0 4px 10px rgba(0, 0, 0, 0.1)',
-
-  }));
-
-
   const isNextTasksEmpty = nextTasks.length === 0;
   const isCompletedTasksEmpty = completedTasks.length === 0;
-  const lastWeek = today.subtract(7, 'day');
 
   return (
     <AppProvider
@@ -305,8 +286,8 @@ export default function DashboardLayoutBasic(props) {
         slots={{
           appTitle: CustomAppTitle,
           sidebarFooter: SidebarFooter,
+
         }}
-        
       >
         <PageContainer>
           <div><CalendarComponent tasks={tasks || []} /></div>
@@ -316,7 +297,7 @@ export default function DashboardLayoutBasic(props) {
                 {/* TODAY */}
                 <Box>
                   <div className="title">
-                    <Typography variant="h5">Today's tasks</Typography>
+                    <Typography variant="h4">Today's tasks</Typography>
                   </div>
                   {todayTasks.map((task, index) => {
                     const taskDate = dayjs(task.date);
@@ -341,7 +322,7 @@ export default function DashboardLayoutBasic(props) {
                           <Typography variant="body2">Date: {taskDate.isValid() ? taskDate.format('YYYY-MM-DD') : 'Invalid date'}</Typography>
                           <Typography variant="body2">Category: {task.category}</Typography>
                         </div>
-                        <DeleteButton onDelete={() => deleteTask(task)} />
+                        <DeleteButton onDelete={() => deleteTask(task)}/>
                       </Task>
                     );
                   })}
@@ -351,7 +332,7 @@ export default function DashboardLayoutBasic(props) {
                 {/* NEXT */}
                 <Box>
                   <div className="title">
-                    <Typography variant="h5">Next Tasks</Typography>
+                    <Typography variant="h4">Next Tasks</Typography>
                     <DatePicker selectedDate={selectedDate} onDateChange={handleDateChange} />
                   </div>
 
@@ -380,7 +361,7 @@ export default function DashboardLayoutBasic(props) {
                             <Typography variant="body2">Date: {taskDate.isValid() ? taskDate.format('YYYY-MM-DD') : 'Invalid date'}</Typography>
                             <Typography variant="body2">Category: {task.category}</Typography>
                           </div>
-                          <DeleteButton onDelete={() => deleteTask(task)} />
+                          <DeleteButton onDelete={() => deleteTask(task)}/>
                         </Task>
                       );
                     })
@@ -392,21 +373,12 @@ export default function DashboardLayoutBasic(props) {
             {router.pathname === '/graphs' && (
 
               <div>
-                <Typography sx={{ marginTop: '5%', marginLeft: '2%', marginBottom: '2%' }} variant="h5">
-                  {lastWeek.format('dddd DD MMM')} &nbsp; - &nbsp; {today.format('dddd DD MMM')} :
-                  </Typography>
                 <Box>
-                  <PieChartCategoriesData tasks={completedTasks} />
-                </Box>
-                <Box>
-                  <LineChartData tasks={completedTasks} />
-                </Box>
-                <Box>
-                  <PieChartTasksData tasks={completedTasks} />
+                <LineChartData tasks={completedTasks} />
                 </Box>
                 <Box>
                   <div className="title">
-                    <Typography sx={{ marginBottom: '2%' }} variant="h5">Completed tasks</Typography>
+                    <Typography variant="h4">Completed tasks <CheckCircleIcon color='success' fontSize="large"/></Typography>
                     <DatePicker selectedDate={selectedDate} onDateChange={handleDateChange} />
                   </div>
 
@@ -439,7 +411,7 @@ export default function DashboardLayoutBasic(props) {
         <Fab
           aria-label="add"
           sx={{
-            position: 'fixed', bottom: 25, right: 50, backgroundColor: '#34B3DB', color: '#FFFFFF',
+            position: 'fixed', bottom: 24, right: 20, backgroundColor: '#34B3DB', color: '#FFFFFF',
             '&:hover': {
               backgroundColor: '#FFFFFF',
               color: '#000000',
